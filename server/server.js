d2e225234e5339e3912b6b9cbf6859a19689af69
lparis42 <56311304+lparis42@users.noreply.github.com>
2024-05-27 18:19:24 +0200

    database update

--- server/server.js
+++ server/server.js
@@ -6,20 +6,26 @@ const fs = require('fs');
 const socketIo = require('socket.io');
 const pgp = require('pg-promise')();
 const bcrypt = require('bcrypt');
-const database = require('./database');
+const Database = require('./database');
 
 class Server {
-  constructor() {
-    this.app = express()
+
+  // Used to start the server
+  async start() {
+    this.app = express();
     this.configureMiddleware();
     this.configureRoutes();
     this.configureHttpsServer();
     this.configureSocketIO();
-    this.configureDatabase();
+    await this.configureDatabase();
+
+    this.server.listen(process.env.PORT, () => {
+      console.log(`${this.constructor.name}: Listening on port ${process.env.PORT}`);
+    });
   }
 
   // Used to configure the middleware
-  configureMiddleware() {
+  async configureMiddleware() {
     this.app.use(cors({
       origin: [`https://localhost:${process.env.PORT}`],
       methods: ['GET', 'POST'],
@@ -27,26 +33,29 @@ class Server {
     }));
     this.app.use(express.json());
     this.app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+    console.log(`${this.constructor.name}: Middleware configured`);
   }
 
   // Used to create the routes
-  configureRoutes() {
+  async configureRoutes() {
     this.app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
     });
+    console.log(`${this.constructor.name}: Routes configured`);
   }
 
   // Used to create the HTTPS server
-  configureHttpsServer() {
+  async configureHttpsServer() {
     this.server = https.createServer({
       key: fs.readFileSync('./server.key'),
       cert: fs.readFileSync('./server.cert'),
       passphrase: 'matcha',
     }, this.app);
+    console.log(`${this.constructor.name}: HTTPS server configured`);
   }
 
   // Used to create the socket server
-  configureSocketIO() {
+  async configureSocketIO() {
     this.io = socketIo(this.server, {
       cors: {
         origin: [`https://localhost:${process.env.PORT}`],
@@ -62,60 +71,29 @@ class Server {
         console.log('User disconnected');
       });
     });
+    console.log(`${this.constructor.name}: Socket.IO configured`);
   }
 
-  // Used to create the database and table
-  configureDatabase() {
-    // Used to create database instance
-    this.db = new database('postgres', 'localhost', 'postgres', 'pg', 5432);
+  // Used to configure the database
+  async configureDatabase() {
+    this.db = new Database('postgres', 'localhost', 'postgres', 'pg', 5432);
 
-    // Used to connect to the database
-    this.db.connect()
-      .then(() => {
-        return this.db.dropTable('users');
-      })
-      .then(() => {
-        const columns = [
-          `id SERIAL PRIMARY KEY`,
-          `username VARCHAR(20) UNIQUE CHECK (char_length(username) BETWEEN 6 AND 20 AND username ~ '^[A-Za-z0-9]+$')`,
-          `password VARCHAR(60)`,
-          `email VARCHAR(50) UNIQUE CHECK (char_length(email) BETWEEN 6 AND 50 AND email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$')`,
-          `last_name VARCHAR(35)`,
-          `first_name VARCHAR(35)`,
-          `date_of_birth DATE CHECK (date_of_birth BETWEEN '1900-01-01' AND '2021-12-31')`,
-          `gender VARCHAR(35) CHECK (gender IN ('Male', 'Female', 'Other'))`,
-          `sexual_orientation VARCHAR(35) CHECK (sexual_orientation IN ('Heterosexual', 'Bisexual', 'Homosexual', 'Other'))`,
-          `biography VARCHAR(255)`,
-          `interests VARCHAR(50)[10]`,
-          `pictures VARCHAR(255)[5] DEFAULT ARRAY['defaut.jpg']::VARCHAR(255)[]`,
-          `created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP`,
-          `updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP`
-        ];
-        return this.db.createTable('users', columns);
-      })
-      .then(() => {
-        const row = {
-          columns: ['username', 'password'],
-          values: ['matcha42', 'matcha42'],
-        };
-        return this.db.insert('users', row);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-
-  // Used to start the server
-  start() {
-    this.server.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
+    await this.db.connect();
+    await this.db.drop('users');
+    const users_columns = require('./variables').User.columns;
+    await this.db.create('users', users_columns);
+    const row = {
+      username: 'matcha42',
+      password: 'matcha42',
+    };
+    await this.db.insert('users', row);
+    console.log(`${this.constructor.name}: Database configured`);
   }
 
   // Used to close the server when testing
   closeServer(done) {
     this.server.close(done);
-    console.log(`Server on port ${process.env.PORT} is closed`);
+    console.log(`${this.constructor.name}: Server closed`);
   }
 }
 
