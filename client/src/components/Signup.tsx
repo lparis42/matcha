c58e8028a1fc0146f83e07ef2877fa9a905de5ee
psemsari <psemsari@student.42.fr>
2024-06-04 01:06:12 +0000

    signup rework and add shadcn

---
+++ client/src/components/Signup.tsx
@@ -0,0 +1,76 @@
+import { HeartIcon } from '@heroicons/react/24/solid'
+import { Button } from './ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import React from 'react'
+import { DatePicker } from './ui/datepicker'
+
+export default function Signup () {
+    return (
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid grid-cols-2 gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="first-name">First name</Label>
+                <Input id="first-name" placeholder="Max" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="last-name">Last name</Label>
+                <Input id="last-name" placeholder="Robinson" required />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="username">Username</Label>
+              <Input
+                id="username"
+                type="username"
+                placeholder="username"
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="birthday">Birthday</Label>
+              <DatePicker />
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <a onClick={() => {}} className="underline">
+              Sign in
+            </a>
+          </div>
+        </CardContent>
+      </Card>
+    )
+}
