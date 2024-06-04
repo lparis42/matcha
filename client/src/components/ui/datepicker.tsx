3c7957a71dccedbc6a802157b57723701c017562
psemsari <psemsari@student.42.fr>
2024-06-04 01:06:12 +0000

    signup rework and add shadcn

--- client/src/components/ui/datepicker.tsx
+++ client/src/components/ui/datepicker.tsx
@@ -1,15 +1,4 @@
 import * as React from "react"
-import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
- 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
 import { Label } from "@/components/ui/label"
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select"
 import { ScrollArea } from "@/components/ui/scroll-area"
