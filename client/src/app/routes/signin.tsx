13d841b453555e9d7394afc4dc68722c945c40dc
psemsari <psemsari@student.42.fr>
2024-06-04 17:14:33 +0000

    profile and signin

---
+++ client/src/app/routes/signin.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Signin from "@/components/Signin"
+
+export function Component() {
+  return (
+    <Signin/>
+  )
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+//import { BrowserRouter as Router } from 'react-router-dom'
+import { Suspense } from 'react'
+import { RouterProvider } from 'react-router-dom'
 import App from './app/App.jsx'
 import './index.css'
+import {router} from '@/router'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <RouterProvider router={router} />
+    </Suspense>
   </React.StrictMode>
 );
