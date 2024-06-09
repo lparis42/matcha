97286c86f542bc38a5ee54e7ff0516940e1b37e4
lparis42 <56311304+lparis42@users.noreply.github.com>
2024-06-09 11:13:38 +0200

    Revert "'update'"
    This reverts commit aa27c50bd95ba52529602fe427a20e37380c324d.

---
+++ client/src/app/routes/signin.tsx
@@ -0,0 +1,72 @@
+import React, {useState} from "react"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Link } from "react-router-dom"
+import { Button } from "@/components/ui/button"
+import { Login, emitLogin } from "@/api/Socket"
+
+export function Component() {
+
+  const [data, setData] = useState<Login>({
+    password: "",
+    username: ""
+  });
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.id]: e.target.value
+    });
+  }
+
+  const handleSubmit = () => {
+    console.log(data);
+    emitLogin(data);
+  }
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+            <Card className="mx-auto max-w-sm">
+                <CardHeader>
+                <CardTitle className="text-2xl">Login</CardTitle>
+                <CardDescription>
+                    Enter your email below to login to your account
+                </CardDescription>
+                </CardHeader>
+                <CardContent>
+                <div className="grid gap-4">
+                    <div className="grid gap-2">
+                    <Label htmlFor="username">Username</Label>
+                    <Input
+                        id="username"
+                        type="username"
+                        placeholder=""
+                        value={data.username} onChange={(e) => handleChange(e)}
+                        required
+                    />
+                    </div>
+                    <div className="grid gap-2">
+                    <div className="flex items-center">
+                        <Label htmlFor="password">Password</Label>
+                        <Link to="#" className="ml-auto inline-block text-sm underline">
+                        Forgot your password?
+                        </Link>
+                    </div>
+                    <Input id="password" type="password" value={data.password} onChange={(e) => handleChange(e)} required />
+                    </div>
+                    <Button type="submit" className="w-full" onClick={() => {handleSubmit()}}>
+                    Login
+                    </Button>
+                </div>
+                <div className="mt-4 text-center text-sm">
+                    Don&apos;t have an account?{" "}
+                    <Link to="/signup" className="underline">
+                    Sign up
+                    </Link>
+                </div>
+                </CardContent>
+            </Card>
+        </div>
+  )
+}
