59bf65387d96578419e67e5147c9c315095e033b
lparis42 <56311304+lparis42@users.noreply.github.com>
2024-06-13 12:45:48 +0200

    'update'

--- server/src/socket.events/handleClientOnline.js
+++ server/src/socket.events/handleClientOnline.js
@@ -6,14 +6,20 @@ async function handleClientOnline(socket, data, cb) {
         if (!session.account) {
             throw { client: 'Cannot check online while not logged in', status: 401 };
         }
-        const { target_account } = data;
-        if (!target_account || typeof target_account !== 'number' || target_account < 1) {
-            throw { client: 'Invalid target account', status: 400 };
+        if (!Array.isArray(data.target_accounts)) {
+            throw { client: 'Invalid target accounts', status: 400 };
         }
-        const target_session = await this.getSessionByAccount(target_account).account;
+        const target_sessions = [];
+        for (const target_account of data.target_accounts) {
+            if (typeof target_account !== 'number' || target_account < 1) {
+                throw { client: `Invalid target account: ${target_account}`, status: 400 };
+            }
+            const target_session = await this.getSessionByAccount(target_account).account;
+            target_sessions.push(target_session);
+        }
 
-        cb(null, target_session);
-        console.log(`\x1b[35m${socket.handshake.sessionID}\x1b[0m:\x1b[34m${socket.id}\x1b[0m - Client online check for account '${target_account}'`);
+        cb(null, target_sessions);
+        console.log(`\x1b[35m${socket.handshake.sessionID}\x1b[0m:\x1b[34m${socket.id}\x1b[0m - Client online check for accounts: ${data.target_accounts}`);
     } catch (err) {
         cb({ message: err.client || 'Internal server error', status: err.status || 500 });
         console.error(`\x1b[35m${socket.handshake.sessionID}\x1b[0m:\x1b[34m${socket.id}\x1b[0m - Online check error: ${err.client || err}`);
