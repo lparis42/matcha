7915ebf04f105a871f389cf47fe1e5a86d8afc21
psemsari <psemsari@student.42.fr>
2024-09-11 02:35:27 +0200

    test store for message

--- client/src/store.tsx
+++ client/src/store.tsx
@@ -1,20 +1,51 @@
 import { StateCreator, create } from 'zustand'
 import { combine, persist } from 'zustand/middleware'
-import { socket } from "@/api/Socket";
-import { io } from 'socket.io-client';
 
-const useSocketStore = create(persist((set) => ({
-    socket: null,
-    connect: () => {
-      set({ socket: socket });
-    },
-    disconnect: (state) => {
-      const { socket } = state.socket;
-      if (socket) {
-        socket.disconnect();
-        set({ socket: null });
-      }
-    },
-  }), {name: 'socket'}));
-  
-  export default useSocketStore;
+interface Message {
+  id: number;
+  avatar: string;
+  name: string;
+  message: string;
+}
+
+interface User {
+  id: number;
+  avatar: string;
+  messages: Message[];
+  name: string;
+}
+
+interface SocketStore {
+  users: User[];
+  addUser: (user: User) => void;
+  addMessage: (userId: number, message: Message) => void;
+}
+
+const useSocketStore = create(
+  persist<SocketStore>(
+    (set, get) => ({
+      users: [],
+
+      addUser: (user: User) => {
+        set((state) => ({
+          users: [...state.users, user],
+        }));
+      },
+      addMessage: (userId: number, message: Message) => {
+        set((state) => ({
+          users: state.users.map((user) =>
+            user.id === userId
+              ? { ...user, messages: [...user.messages, message] }
+              : user
+          ),
+        }));
+      },
+  }),
+  {
+    name: 'chat-storage',
+    getStorage: () => localStorage
+  }
+
+));
+
+export default useSocketStore;
