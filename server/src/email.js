97f337b682613126de9003b839f70ca89433effc
lparis42 <56311304+lparis42@users.noreply.github.com>
2024-09-30 15:08:57 +0200

    'update'

--- server/src/email.js
+++ server/src/email.js
@@ -1,55 +1,62 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
+const querystring = require('querystring');
+const axios = require('axios');
 require('dotenv').config();
+
+const TOKEN_URL = 'https://oauth2.googleapis.com/token';
 
 class Email {
   constructor() {
     console.log('Initializing OAuth2 Client with:');
 
-    this.oAuth2Client = new google.auth.OAuth2(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      process.env.REDIRECT_URI
-    );
+    //this.getTokensWithCode('');
+    this.initializeTransporter();
 
-    // Définir les informations d'authentification avec le refresh token
-    this.oAuth2Client.setCredentials({
-      refresh_token: process.env.REFRESH_TOKEN
-    });
+  }
 
-    // Initialiser le transporteur de mail
-    this.initializeTransporter().then(async () => {
-      // Envoi d'un email de test
-      console.log('Sending test email...');
-      const recipients = {
-        from: process.env.GMAIL_USER,
-        to: process.env.GMAIL_USER,
-        subject: 'Test email',
-        text: 'This is a test email.'
-      };
+  async getTokensWithCode(code) {
+    try {
+      const data = querystring.stringify({
+        code: code,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        redirect_uri: process.env.REDIRECT_URI,
+        grant_type: 'authorization_code',
+      });
 
-      try {
-        const info = await this.transporter.sendMail(recipients);
-        console.log('Email sent: (' + info.response + ') to ' + recipients.to);
-      } catch (error) {
-        console.error('Error sending email:', error.response);
-      }
-    });
+      const response = await axios.post(TOKEN_URL, data, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+      const accessToken = response.data.access_token;
+      const refreshToken = response.data.refresh_token;
+
+      console.log('Access Token:', accessToken);
+      console.log('Refresh Token:', refreshToken);
+
+      return { accessToken, refreshToken };
+    } catch (error) {
+      console.error(error.response.data);
+    }
   }
 
   async initializeTransporter() {
+
     try {
-      // Obtenir un access token à partir du refresh token
+      this.oAuth2Client = new google.auth.OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URI
+      );
+
+      this.oAuth2Client.setCredentials({
+        refresh_token: process.env.REFRESH_TOKEN
+      });
+
       const accessToken = (await this.oAuth2Client.getAccessToken()).token;
 
-      // console.log('GMAIL_USER:', process.env.GMAIL_USER);
-      // console.log('ACCESS_TOKEN:', accessToken);
-      // console.log('CLIENT_ID:', process.env.CLIENT_ID);
-      // console.log('CLIENT_SECRET:', process.env.CLIENT_SECRET);
-      // console.log('REDIRECT_URI:', process.env.REDIRECT_URI);
-      // console.log('REFRESH_TOKEN:', process.env.REFRESH_TOKEN);
-
-      // Création du transporteur nodemailer avec OAuth2
       this.transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -62,6 +69,16 @@ class Email {
         },
       });
 
+      console.log('Sending test email...');
+      const recipients = {
+        from: process.env.GMAIL_USER,
+        to: process.env.GMAIL_USER,
+        subject: 'Test email',
+        text: 'This is a test email.'
+      };
+      const info = await this.transporter.sendMail(recipients);
+      console.log('Email sent: (' + info.response + ') to ' + recipients.to);
+
     } catch (error) {
       console.error('Error creating OAuth2 transporter:', error);
       throw error;
@@ -69,7 +86,6 @@ class Email {
   }
 
   async post(recipients) {
-    return;
     try {
       const info = await this.transporter.sendMail(recipients);
       console.log('Email sent: (' + info.response + ') to ' + recipients.to);
@@ -77,6 +93,7 @@ class Email {
       console.error('Error sending email:', error.response);
     }
   }
+
 }
 
 module.exports = Email;
